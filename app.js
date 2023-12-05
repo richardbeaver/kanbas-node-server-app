3658c9138db1812a3f5f453d17cf4e654ad17597
Richard Beaver <rickybeaver3@gmail.com>
2023-12-04 21:50:14 -0500

    a6

--- app.js
+++ app.js
@@ -1,19 +1,52 @@
-// const express = require("express");
 import express from "express";
+import EnrollmentRoutes from "./enrollments/routes.js";
 import HelloRoutes from "./hello.js";
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
+import UserRoutes from "./users/routes.js";
+import mongoose from "mongoose";
+import LikesRoutes from "./likes/routes.js";
+import FollowsRoutes from "./follows/routes.js";
+import SectionRoutes from "./sections/routes.js";
 import cors from "cors";
+import session from "express-session";
 import "dotenv/config";
 
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
+
 app.use(express.json());
 
+FollowsRoutes(app);
+LikesRoutes(app);
+UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
 HelloRoutes(app);
+SectionRoutes(app);
+EnrollmentRoutes(app);
 
-app.listen(process.env.BACKEND_PORT || 4000);
+app.listen(4000);
--- modules/routes.js
+++ modules/routes.js
@@ -1,17 +1,16 @@
 import Database from "../Database/index.js";
-
+import * as dao from "./dao.js";
 function ModuleRoutes(app) {
   app.get("/api/modules", (req, res) => {
     const modules = Database.modules;
     res.json(modules);
   });
-
-  app.get("/api/courses/:id/modules", (req, res) => {
+  app.get("/api/courses/:id/modules", async (req, res) => {
     const { id } = req.params;
-    const modules = Database.modules.filter((module) => module.course === id);
+    // const modules = Database.modules.filter((module) => module.course === id);
+    const modules = await dao.findModulesForCourse(id);
     res.json(modules);
   });
-
   app.get("/api/modules/:id", (req, res) => {
     const { id } = req.params;
     const module = Database.modules.find((module) => module._id === id);
@@ -21,7 +20,6 @@ function ModuleRoutes(app) {
     }
     res.json(module);
   });
-
   app.delete("/api/modules/:id", (req, res) => {
     const { id } = req.params;
     const index = Database.modules.findIndex((module) => module._id === id);
@@ -30,19 +28,20 @@ function ModuleRoutes(app) {
       return;
     }
     Database.modules.splice(index, 1);
-    res.sendStatus(204);
+    res.json(204);
   });
+  app.post("/api/courses/:cid/modules", async (req, res) => {
+    // const newModule = {
+    //   ...req.body,
+    //   course: req.params.cid,
+    //   _id: new Date().getTime().toString(),
+    // };
+    // Database.modules.unshift(newModule);
 
-  app.post("/api/courses/:cid/modules", (req, res) => {
-    const newModule = {
-      ...req.body,
-      course: req.params.cid,
-      _id: new Date().getTime().toString(),
-    };
-    Database.modules.unshift(newModule);
+    const { cid } = req.params;
+    const newModule = await dao.createModuleForCourse(cid, req.body);
     res.json(newModule);
   });
-
   app.put("/api/modules/:id", (req, res) => {
     const { id } = req.params;
     const index = Database.modules.findIndex((module) => module._id === id);
@@ -57,5 +56,4 @@ function ModuleRoutes(app) {
     res.json(200);
   });
 }
-
 export default ModuleRoutes;
